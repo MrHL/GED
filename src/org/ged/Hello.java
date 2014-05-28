2d824865e8e9b79ee5f5c3dfd8fb93f7161568d0
javalusir <javalusir@163.com>
2014-05-28 15:04:55 +0800

    test again

--- src/org/ged/Hello.java
+++ src/org/ged/Hello.java
@@ -9,5 +9,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("liguo ");
+		System.out.println("This test Again");
 	}
 }
